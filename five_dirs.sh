d58b2178028b18ceba8e7b0f19574f6da73dca84
Jack Owen <jhowen2@wisc.edu>
2022-03-08 20:08:59 -0600

    Created five_dirs.sh file. Jack Owen and Kelsey Gelden.

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+mkdir five
+
+for i in {1..5}; do
+    mkdir ./five/dir${i}
+    for j in {1..4}; do
+	touch ./five/dir${i}/file${j}.txt
+	for (( k=1; k <= $j; ++k )); do
+	    echo $j >> ./five/dir${i}/file${j}.txt
+	done
+    done
+done
