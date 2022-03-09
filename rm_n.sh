baa10506324058154ccb59327911d58ce7e3986c
Jack Owen <jhowen2@wisc.edu>
2022-03-09 11:31:26 -0600

    Removed extra comments. Jack

--- rm_n.sh
+++ rm_n.sh
@@ -8,20 +8,3 @@ fi
 cd ~/Desktop/linuxExercises/$1
 files=$(find ./ -size +$2)
 rm -r $files
-
-
-
-#for i in $(ls); do
-    #echo $i
-    #files=$(find $i -size +4)
-    #rm -r files
-    #echo $files
-    #cd ~/Desktop/linuxExercises/$1/$i
-    #ls
-    #files=$(find files -size +4)
-    #echo $files
-   #for j in $(ls); do
-	#txt=$(find ./ -size +3)
-	#rm -r $txt
-    #done
-#done
