c8a3651892c89eaf83ce4b10cfc9fa173bced599
Jack Owen <jhowen2@wisc.edu>
2022-03-09 14:38:42 -0600

    Editted mean.sh. Jack

--- mean.sh
+++ mean.sh
@@ -4,27 +4,28 @@ function mean {
     local col=$1
     local file=$2
     
-    x=$(find ~/Desktop/linuxExercises $file | cut -d ',' -f $col)
-    
     count=0
     total=0
-
-    for i in $x
-    do
-	#total=$(echo $total+$i | bc)
-	#((count++))
-	echo $i
-    done
-
-    avg=$(( $total / $count ))
-
+    skip=0
+    
+    while read -r line; do
+	if (( skip == 0 )); then
+            skip=1
+	else
+	    x=$(echo $line | cut -d "," -f $col)
+	    total=$(($total+$x))
+	    ((count++))
+	fi
+    done < $file
+    
+    avg=$(($total/$count))
     echo $avg
 }
 
-
 if (( $# == 1 )); then
-    #Supposed to deal with taking in only 1 argument, will edit later.
-elif(( $# == 2 )); then
+    file="mtcars.csv"
+    mean $1 $file
+elif (( $# == 2 )); then
     mean $1 $2
 else
     echo "usage: $0 <column> [file.csv]"
